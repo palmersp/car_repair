2cbf28bfb8995ccdf33fec3e1ad17d8eb0f6be6a
Spencer Palmer <palmersp@spencerpalmer.com>
2018-03-13 06:49:19 -0600

    Reviews are destroyed if appointment is destroyed.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Appointment < ActiveRecord::Base
-  has_one :review
+  has_one :review, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
